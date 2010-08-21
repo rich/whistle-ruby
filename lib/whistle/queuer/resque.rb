0b9a7cfcdc60b0ce900af890645c54df26b9de43
Rich Cavanaugh <rich@withoutscope.com>
2010-08-21 18:59:30 -0400

    our Resque class doesn't handle #enqueue

--- lib/whistle/queuer/resque.rb
+++ lib/whistle/queuer/resque.rb
@@ -2,7 +2,7 @@ class Whistle::Queuer::Resque < Whistle::Queuer
   @queue = 'whistle_notices'
 
   def notify(source, action, params)
-    Resque.enqueue(self.class, source, action, params)
+    ::Resque.enqueue(self.class, source, action, params)
   end
 
   def self.perform(source, action, params)
